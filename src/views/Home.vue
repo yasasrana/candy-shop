ef17cc73fb39168616e98105ef9fb23e39b09b26
yasasrana <81549051+yasasrana@users.noreply.github.com>
2024-01-16 00:20:44 +0530

    changes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,188 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Vue JS and Vite"/>
-    <h1 class="text-3xl text-red-700 font-bold underline">heloooooo</h1>
+    <div className="bg-[#f2f5ff] flex flex-row justify-center w-full">
+      <div className="bg-[#f2f5ff] overflow-hidden w-[1440px] h-[3223px]">
+        <div className="relative h-[3883px]">
+          <div className="h-[1719px] top-[2164px] [background:linear-gradient(180deg,rgb(198,238,253)_0%,rgba(230,111,175,0.41)_100%)] absolute w-[1440px] left-0" />
+          <img
+            className="absolute w-[1440px] h-[1178px] top-[1186px] left-0"
+            alt="Union"
+            src="../assets/DesignArts/svg/union.svg"
+          />
+          <img
+            className="h-[611px] top-0 object-cover absolute w-[1440px] left-0"
+            alt="Rectangle"
+            src="../assets/photos/Home.jpg"
+          />
+          <div className="absolute top-[53px] left-[780px] [text-shadow:7px_7px_6px_#00000059] [font-family:'Boodle-Black-Regular',Helvetica] font-normal text-white text-[96px] tracking-[0] leading-[normal]">
+            CANDY SHOP
+          </div>
+          <p className="absolute top-[128px] left-[900px] [font-family:'Boodle-Black-Regular',Helvetica] font-normal text-[#0a2d6a] text-[45px] text-right tracking-[0] leading-[normal]">
+            Welcome To Sally
+            <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Candy Shop
+          </p>
+          <img
+            className="absolute w-[1440px] h-[366px] top-[1274px] left-0"
+            alt="Vector"
+            src="../assets/DesignArts/svg/vector1.svg"
+          />
+          <img
+            className="absolute w-[1440px] h-[911px] top-[419px] left-0"
+            alt="Vector"
+            src="../assets/DesignArts/svg/vector2.svg"
+          />
+          <p className="absolute w-[305px] top-[263px] left-[932px] [font-family:'Boodle-Black-Regular',Helvetica] font-normal text-[#0a2d6a] text-[12px] text-right tracking-[0] leading-[normal]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id mauris vel ligula vulputate cursus. Sed
+            nec felis nec orci vestibulum auctor. Fusce euismod est vel ipsum laoreet, non aliquam libero suscipit.
+          </p>
+          <p className="w-[585px] top-[874px] left-[166px] text-[#69696b] text-[20px] absolute [font-family:'Actor',Helvetica] font-normal tracking-[0] leading-[normal]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id mauris vel ligula vulputate cursus. Sed
+            nec felis nec orci vestibulum auctor. Fusce euismod est vel ipsum laoreet, non aliquam libero suscipit.
+          </p>
+          <p className="w-[585px] top-[989px] left-[169px] text-[#696a6b] text-[20px] absolute [font-family:'Actor',Helvetica] font-normal tracking-[0] leading-[normal]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id mauris vel ligula vulputate cursus. Sed
+            nec felis nec orci vestibulum auctor. Fusce euismod est vel ipsum laoreet, non aliquam libero suscipit.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id mauris vel ligula vulputate cursus. Sed
+            nec felis nec orci vestibulum auctor. Fusce euismod est vel ipsum laoreet, non aliquam libero suscipit.
+          </p>
+          <div className="absolute top-[654px] left-[165px] [text-shadow:7px_7px_10px_#00000026] [font-family:'Boodle-Black-Regular',Helvetica] font-normal text-white text-[96px] tracking-[0] leading-[normal]">
+            OUR&nbsp;&nbsp;STORY
+          </div>
+          <div className="absolute top-[1595px] left-[503px] [text-shadow:7px_7px_10px_#00000026] [font-family:'Boodle-Black-Regular',Helvetica] font-normal text-white text-[96px] tracking-[0] leading-[normal]">
+            SERVICES
+          </div>
+          <div className="absolute top-[2388px] left-[418px] [text-shadow:7px_7px_10px_#00000026] [font-family:'Boodle-Black-Regular',Helvetica] font-normal text-white text-[96px] tracking-[0] leading-[normal]">
+            COME VISIT US!
+          </div>
+          <p className="absolute w-[682px] top-[2554px] left-[379px] [text-shadow:7px_7px_10px_#00000026] [font-family:'Boodle-Black-Regular',Helvetica] font-normal text-white text-[22px] text-center tracking-[0] leading-[normal]">
+            Corem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id mauris vel ligula vulputate cursus. .
+          </p>
+          <img
+            className="absolute w-[330px] h-[604px] top-[675px] left-[920px] object-cover"
+            alt="Candy shop"
+            src="../assets/photos/candy shop.png"
+          />
+          <img
+            className="w-[172px] top-[1175px] left-[159px] absolute h-[72px]"
+            alt="Group"
+            src="https://c.animaapp.com/PPRW07mB/img/group-5@2x.png"
+          />
+          <img
+            className="w-[290px] top-[2796px] left-[614px] absolute h-[72px]"
+            alt="Group"
+            src="https://c.animaapp.com/PPRW07mB/img/group-6@2x.png"
+          />
+          <div className="absolute top-[1670px] left-[560px] [font-family:'Boodle-Black-Regular',Helvetica] font-normal text-[#0a2d6a] text-[45px] text-right tracking-[0] leading-[normal]">
+            What We Offer
+          </div>
+          <div className="absolute top-[2463px] left-[524px] [font-family:'Boodle-Black-Regular',Helvetica] font-normal text-[#0a2d6a] text-[45px] text-right tracking-[0] leading-[normal]">
+            Taste The Delisious
+          </div>
+          <div className="absolute top-[2658px] left-[241px] [font-family:'Boodle-Black-Regular',Helvetica] font-normal text-[#0a2d6a] text-[25px] text-center tracking-[0] leading-[normal]">
+            584-869-8956
+            <br />
+            info@candy.com
+          </div>
+          <p className="absolute top-[2658px] left-[954px] [font-family:'Boodle-Black-Regular',Helvetica] font-normal text-[#0a2d6a] text-[25px] text-center tracking-[0] leading-[normal]">
+            Open 10am-5pm Ever Day
+            <br />
+            Close on Holidays
+          </p>
+          <p className="absolute w-[290px] top-[2658px] left-[580px] [font-family:'Boodle-Black-Regular',Helvetica] font-normal text-[#0a2d6a] text-[25px] text-center tracking-[0] leading-[normal]">
+            1234 Candy Rd, San Fransisico ,CA 587964
+          </p>
+          <img
+            className="absolute w-[340px] h-[309px] top-[1770px] left-[137px]"
+            alt="Vector"
+            src="https://c.animaapp.com/PPRW07mB/img/vector-1.svg"
+          />
+          <img
+            className="absolute w-[324px] h-[332px] top-[1756px] left-[538px]"
+            alt="Vector"
+            src="https://c.animaapp.com/PPRW07mB/img/vector-2.svg"
+          />
+          <img
+            className="absolute w-[340px] h-[309px] top-[1770px] left-[909px]"
+            alt="Vector"
+            src="https://c.animaapp.com/PPRW07mB/img/vector-3.svg"
+          />
+          <img
+            className="absolute w-[220px] h-[136px] top-[1801px] left-[160px] object-cover"
+            alt="Candy"
+            src="../assets/photos/candy shop8.png"
+          />
+          <div className="absolute top-[1923px] left-[624px] [font-family:'Boodle-Black-Regular',Helvetica] font-normal text-[#0a2d6a] text-[20px] text-right tracking-[0] leading-[normal]">
+            Birthday Parties
+          </div>
+          <p className="w-[247px] top-[1958px] left-[571px] text-[#69696b] text-[15px] text-center absolute [font-family:'Actor',Helvetica] font-normal tracking-[0] leading-[normal]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id mauris vel ligula vulputate cursus. .
+          </p>
+          <div className="absolute top-[1923px] left-[1018px] [font-family:'Boodle-Black-Regular',Helvetica] font-normal text-[#0a2d6a] text-[20px] text-right tracking-[0] leading-[normal]">
+            Private Functions
+          </div>
+          <p className="w-[247px] top-[1958px] left-[981px] text-[#69696b] text-[15px] text-center absolute [font-family:'Actor',Helvetica] font-normal tracking-[0] leading-[normal]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id mauris vel ligula vulputate cursus. .
+          </p>
+          <div className="absolute top-[1916px] left-[244px] [font-family:'Boodle-Black-Regular',Helvetica] font-normal text-[#0a2d6a] text-[20px] text-right tracking-[0] leading-[normal]">
+            Sweet Shop
+          </div>
+          <p className="w-[247px] top-[1951px] left-[169px] text-[#69696b] text-[15px] text-center absolute [font-family:'Actor',Helvetica] font-normal tracking-[0] leading-[normal]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id mauris vel ligula vulputate cursus. .
+          </p>
+          <img
+            className="absolute w-[109px] h-[122px] top-[1801px] left-[639px] object-cover"
+            alt="Candy"
+            src="../assets/photos/candy1.png"
+          />
+          <img
+            className="absolute w-[113px] h-[144px] top-[1790px] left-[1018px] object-cover"
+            alt="Candy"
+            src="../assets/photos/candy2.png"
+          />
+          <img
+            className="absolute w-[37px] h-[47px] top-[2792px] left-[1072px]"
+            alt="Vector"
+            src="https://c.animaapp.com/PPRW07mB/img/vector-8.svg"
+          />
+          <img
+            className="absolute w-[49px] h-[39px] top-[2834px] left-[296px]"
+            alt="Vector"
+            src="https://c.animaapp.com/PPRW07mB/img/vector-11.svg"
+          />
+          <img
+            className="absolute w-[25px] h-[33px] top-[2431px] left-[222px]"
+            alt="Vector"
+            src="https://c.animaapp.com/PPRW07mB/img/vector-9.svg"
+          />
+          <img
+            className="absolute w-[27px] h-[25px] top-[2335px] left-[1119px]"
+            alt="Vector"
+            src="https://c.animaapp.com/PPRW07mB/img/vector-10.svg"
+          />
+          <img
+            className="absolute w-[27px] h-[25px] top-[2512px] left-[924px]"
+            alt="Vector"
+            src="https://c.animaapp.com/PPRW07mB/img/vector-12.svg"
+          />
+          <img
+            className="absolute w-[25px] h-[27px] top-[2669px] left-[516px]"
+            alt="Vector"
+            src="https://c.animaapp.com/PPRW07mB/img/vector-13.svg"
+          />
+          <div className="absolute top-[782px] left-[165px] [font-family:'Boodle-Black-Regular',Helvetica] font-normal text-[#0a2d6a] text-[45px] text-right tracking-[0] leading-[normal]">
+            Established in 1967
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '../components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+};
 </script>
+
+
