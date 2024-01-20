f1cca38a8918d0b81f3a170977d4b8ad2f8d49fc
yasasrana <81549051+yasasrana@users.noreply.github.com>
2024-01-21 00:11:13 +0530

    changes

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,138 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <div className="bg-[#f2f5ff] flex flex-row justify-center w-full">
+      <div className="bg-[#f2f5ff] overflow-hidden w-[1440px] h-[2768px]">
+        <div className="relative w-[2036px] h-[3256px] left-[-564px]">
+          <div className="absolute w-[1440px] h-[1349px] top-[1907px] left-[564px] [background:linear-gradient(180deg,rgb(198,238,253)_0%,rgba(230,111,175,0.41)_100%)]" />
+          <img
+            className="absolute w-[1440px] h-[1948px] top-[301px] left-[564px]"
+            alt="Union"
+            src="https://c.animaapp.com/twwqjW46/img/union.svg"
+          />
+          <img
+            className="h-[327px] top-[590px] left-[564px] absolute w-[1440px]"
+            alt="Vector"
+            src="https://c.animaapp.com/twwqjW46/img/vector.svg"
+          />
+          <div className="absolute w-[320px] h-[320px] top-[1460px] left-[1133px] bg-[#d9d9d9] rounded-[20px] shadow-[0px_4px_4px_#00000040]" />
+          <img
+            className="absolute w-[1440px] h-[581px] top-0 left-[564px]"
+            alt="Rectangle"
+            src="https://c.animaapp.com/twwqjW46/img/rectangle-31.png"
+          />
+          <div className="absolute top-[110px] left-[1424px] [text-shadow:7px_7px_6px_#00000059] [font-family:'Boodle-Black-Regular',Helvetica] font-normal text-white text-[96px] tracking-[0] leading-[normal]">
+            PRODUCTS
+          </div>
+          <div className="top-[176px] left-[1428px] text-[45px] text-right absolute [font-family:'Boodle-Black-Regular',Helvetica] font-normal text-[#0a2d6a] tracking-[0] leading-[normal]">
+            Our Speciality Sweets
+          </div>
+          <div className="w-[295px] top-[1346px] left-[746px] text-[24px] absolute [font-family:'Boodle-Black-Regular',Helvetica] font-normal text-[#0a2d6a] tracking-[0] leading-[normal]">
+            Our Speciality Candies
+          </div>
+          <div className="top-[1390px] left-[746px] absolute w-[295px] [font-family:'Boodle-Black-Regular',Helvetica] font-normal text-[#f4346c] text-[24px] tracking-[0] leading-[normal]">
+            Rs.2500
+          </div>
+          <div className="absolute w-[320px] h-[320px] top-[1002px] left-[734px] bg-[#f2f5ff] rounded-[20px] shadow-[0px_4px_4px_#00000040]" />
+          <div className="top-[1346px] left-[1531px] absolute w-[295px] [font-family:'Boodle-Black-Regular',Helvetica] font-normal text-[#0a2d6a] text-[24px] tracking-[0] leading-[normal]">
+            Our Speciality Candies
+          </div>
+          <div className="top-[1390px] left-[1531px] absolute w-[295px] [font-family:'Boodle-Black-Regular',Helvetica] font-normal text-[#f4346c] text-[24px] tracking-[0] leading-[normal]">
+            Rs.2500
+          </div>
+          <div className="absolute w-[320px] h-[320px] top-[1002px] left-[1519px] bg-[#d9d9d9] rounded-[20px] shadow-[0px_4px_4px_#00000040]" />
+          <div className="top-[1346px] left-[1145px] absolute w-[295px] [font-family:'Boodle-Black-Regular',Helvetica] font-normal text-[#0a2d6a] text-[24px] tracking-[0] leading-[normal]">
+            Our Speciality Candies
+          </div>
+          <div className="top-[1390px] left-[1145px] absolute w-[295px] [font-family:'Boodle-Black-Regular',Helvetica] font-normal text-[#f4346c] text-[24px] tracking-[0] leading-[normal]">
+            Rs.2500
+          </div>
+          <div className="absolute w-[320px] h-[320px] top-[1002px] left-[1133px] bg-[#d9d9d9] rounded-[20px] shadow-[0px_4px_4px_#00000040]" />
+          <img
+            className="h-[366px] top-[618px] left-[564px] absolute w-[1440px]"
+            alt="Vector"
+            src="https://c.animaapp.com/twwqjW46/img/vector-4.svg"
+          />
+          <div className="top-[1804px] left-[746px] absolute w-[295px] [font-family:'Boodle-Black-Regular',Helvetica] font-normal text-[#0a2d6a] text-[24px] tracking-[0] leading-[normal]">
+            Our Speciality Candies
+          </div>
+          <div className="top-[1848px] left-[746px] absolute w-[295px] [font-family:'Boodle-Black-Regular',Helvetica] font-normal text-[#f4346c] text-[24px] tracking-[0] leading-[normal]">
+            Rs.2500
+          </div>
+          <div className="absolute w-[320px] h-[320px] top-[1460px] left-[734px] bg-[#d9d9d9] rounded-[20px] shadow-[0px_4px_4px_#00000040]" />
+          <div className="top-[1804px] left-[1531px] absolute w-[295px] [font-family:'Boodle-Black-Regular',Helvetica] font-normal text-[#0a2d6a] text-[24px] tracking-[0] leading-[normal]">
+            Our Speciality Candies
+          </div>
+          <div className="top-[1848px] left-[1531px] absolute w-[295px] [font-family:'Boodle-Black-Regular',Helvetica] font-normal text-[#f4346c] text-[24px] tracking-[0] leading-[normal]">
+            Rs.2500
+          </div>
+          <div className="absolute w-[320px] h-[320px] top-[1460px] left-[1519px] bg-[#d9d9d9] rounded-[20px] shadow-[0px_4px_4px_#00000040]" />
+          <div className="top-[1804px] left-[1145px] absolute w-[295px] [font-family:'Boodle-Black-Regular',Helvetica] font-normal text-[#0a2d6a] text-[24px] tracking-[0] leading-[normal]">
+            Our Speciality Candies
+          </div>
+          <div className="absolute w-[76px] top-[1906px] left-[1729px] [font-family:'Boodle-Black-Regular',Helvetica] font-normal text-[#0a2d6a] text-[24px] tracking-[0] leading-[normal]">
+            1....10
+          </div>
+          <div className="top-[1848px] left-[1145px] absolute w-[295px] [font-family:'Boodle-Black-Regular',Helvetica] font-normal text-[#f4346c] text-[24px] tracking-[0] leading-[normal]">
+            Rs.2500
+          </div>
+          <img
+            className="absolute w-[33px] h-[33px] top-[1905px] left-[1794px]"
+            alt="Forward"
+            src="https://c.animaapp.com/twwqjW46/img/forward@2x.png"
+          />
+          <div className="absolute w-[2036px] h-[535px] top-[353px] left-0">
+            <div className="relative w-[1440px] h-[276px] left-[564px] bg-[url(https://c.animaapp.com/twwqjW46/img/vector-3.svg)] bg-[100%_100%]">
+              <img
+                className="h-[268px] top-[8px] left-0 absolute w-[1440px]"
+                alt="Vector"
+                src="https://c.animaapp.com/twwqjW46/img/vector-2.svg"
+              />
+              <img
+                className="h-[196px] top-[80px] left-0 absolute w-[1440px]"
+                alt="Vector"
+                src="https://c.animaapp.com/twwqjW46/img/vector-1.svg"
+              />
+            </div>
+          </div>
+          <p className="absolute w-[305px] top-[263px] left-[1526px] [font-family:'Boodle-Black-Regular',Helvetica] font-normal text-[#0a2d6a] text-[12px] text-right tracking-[0] leading-[normal]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id mauris vel ligula vulputate cursus. Sed
+            nec felis nec orci vestibulum auctor. Fusce euismod est vel ipsum laoreet, non aliquam libero suscipit.
+          </p>
+          <div className="absolute top-[519px] left-[1162px] [font-family:'Boodle-Black-Regular',Helvetica] font-normal text-[#0a2d6a] text-[45px] text-right tracking-[0] leading-[normal]">
+            Categories
+          </div>
+          <img
+            className="absolute w-[156px] h-[156px] top-[600px] left-[1308px] object-cover"
+            alt="Rectangle"
+            src="https://c.animaapp.com/twwqjW46/img/rectangle-20@2x.png"
+          />
+          <img
+            className="absolute w-[156px] h-[156px] top-[600px] left-[1506px]"
+            alt="Rectangle"
+            src="https://c.animaapp.com/twwqjW46/img/rectangle-21@2x.png"
+          />
+          <img
+            className="absolute w-[156px] h-[156px] top-[608px] left-[1110px]"
+            alt="Rectangle"
+            src="https://c.animaapp.com/twwqjW46/img/rectangle-18@2x.png"
+          />
+          <img
+            className="absolute w-[145px] h-[116px] top-[628px] left-[912px] object-cover"
+            alt="Candy"
+            src="https://c.animaapp.com/twwqjW46/img/candy-shop7-1@2x.png"
+          />
+          <div className="absolute w-[144px] top-[768px] left-[912px] [font-family:'Boodle-Black-Regular',Helvetica] font-normal text-[#0a2d6a] text-[24px] text-center tracking-[0] leading-[normal] whitespace-nowrap">
+            Chocaltes
+          </div>
+          <div className="absolute w-[144px] top-[768px] left-[1110px] [font-family:'Boodle-Black-Regular',Helvetica] font-normal text-[#0a2d6a] text-[24px] text-center tracking-[0] leading-[normal] whitespace-nowrap">
+            Candy
+          </div>
+          <div className="absolute w-[144px] top-[768px] left-[1314px] [font-family:'Boodle-Black-Regular',Helvetica] font-normal text-[#0a2d6a] text-[24px] text-center tracking-[0] leading-[normal] whitespace-nowrap">
+            Jelly Beans
+          </div>
+          <div className="absolute w-[144px] top-[768px] left-[1512px] [font-family:'Boodle-Black-Regular',Helvetica] font-normal text-[#0a2d6a] text-[24px] text-center tracking-[0] leading-[normal] whitespace-nowrap">
+            Licorice
+          </div>
+        </div>
+      </div>
+    </div>
 </template>
--- src/views/SignOut.vue
+++ src/views/SignOut.vue
@@ -1,7 +1,77 @@
 <template>
-  <div class="home">
-    <h1 class="text-3xl text-red-700 font-bold underline">Sign out view</h1>
-  </div>
+  <div className="bg-[#f2f5ff] flex flex-row justify-center w-full">
+      <div className="bg-[#f2f5ff] w-[1440px] h-[895px]">
+        <div className="relative h-[663px] top-[90px]">
+          <img
+            className="absolute w-[1440px] h-[317px] top-[67px] left-0"
+            alt="Vector"
+            src="https://c.animaapp.com/CY8cwYGc/img/vector.svg"
+          />
+          <img
+            className="absolute w-[1440px] h-[272px] top-[339px] left-0"
+            alt="Vector"
+            src="https://c.animaapp.com/CY8cwYGc/img/vector-1.svg"
+          />
+          <img
+            className="absolute w-[1440px] h-[662px] top-0 left-0"
+            alt="Union"
+            src="https://c.animaapp.com/CY8cwYGc/img/union.svg"
+          />
+          <div className="left-[296px] absolute w-[127px] h-[122px] top-[541px] rounded-[20px] border border-solid border-black" />
+          <div className="left-[442px] absolute w-[127px] h-[122px] top-[541px] rounded-[20px] border border-solid border-black" />
+          <img
+            className="absolute w-[375px] h-[476px] top-[22px] left-[213px] object-cover"
+            alt="Element s"
+            src="https://c.animaapp.com/CY8cwYGc/img/2201-i518-025-s-m005-c12-1@2x.png"
+          />
+          <p className="absolute top-[205px] left-[735px] [font-family:'Boodle-Black-Regular',Helvetica] font-normal text-[#0a2d6a] text-[45px] text-right tracking-[0] leading-[normal]">
+            Keto Bars - 12 Bars
+          </p>
+          <p className="absolute w-[540px] top-[283px] left-[741px] [font-family:'Actor',Helvetica] font-normal text-[#001f3eb0] text-[20px] tracking-[0] leading-[normal]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id mauris vel ligula vulputate cursus. Sed
+            nec felis nec orci vestibulum auctor. Fusce euismod est vel ipsum laoreet, non aliquam libero suscipit.
+          </p>
+          <div className="absolute w-[295px] top-[423px] left-[735px] [font-family:'Boodle-Black-Regular',Helvetica] font-normal text-[#f4346c] text-[48px] tracking-[0] leading-[normal] whitespace-nowrap">
+            Rs.2500
+          </div>
+          <img
+            className="absolute w-[40px] h-[50px] top-[495px] left-[1335px]"
+            alt="Vector"
+            src="https://c.animaapp.com/CY8cwYGc/img/vector-4.svg"
+          />
+          <img
+            className="absolute w-[45px] h-[46px] top-[568px] left-[626px]"
+            alt="Vector"
+            src="https://c.animaapp.com/CY8cwYGc/img/vector-5.svg"
+          />
+          <img
+            className="absolute w-[59px] h-[60px] top-[466px] left-[919px]"
+            alt="Vector"
+            src="https://c.animaapp.com/CY8cwYGc/img/vector-6.svg"
+          />
+          <img
+            className="absolute w-[59px] h-[60px] top-[71px] left-[1318px]"
+            alt="Vector"
+            src="https://c.animaapp.com/CY8cwYGc/img/vector-7.svg"
+          />
+          <img
+            className="absolute w-[38px] h-[48px] top-[153px] left-[132px]"
+            alt="Vector"
+            src="https://c.animaapp.com/CY8cwYGc/img/vector-8.svg"
+          />
+          <img
+            className="absolute w-[29px] h-[36px] top-[487px] left-[169px]"
+            alt="Vector"
+            src="https://c.animaapp.com/CY8cwYGc/img/vector-9.svg"
+          />
+          <img
+            className="absolute w-[182px] h-[75px] top-[484px] left-[725px]"
+            alt="Group"
+            src="https://c.animaapp.com/CY8cwYGc/img/group-5@2x.png"
+          />
+        </div>
+      </div>
+    </div>
 </template>
 
 <!-- <script>
